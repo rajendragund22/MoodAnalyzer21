378d847f537a73650c768e90ef54de944482e45f
RajendraGund <rajendragund2015@gmail.com>
2021-09-19 22:26:11 +0530

    Added : Program for the to pass the test case in try catch

--- src/main/java/com/bridgelabz/exception/MoodAnalysisException.java
+++ src/main/java/com/bridgelabz/exception/MoodAnalysisException.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.exception;
 
 public class MoodAnalysisException extends Exception {
+    public Exception_Type type;
+
     public MoodAnalysisException(String message, Exception_Type type) {
         super(message);
+        this.type = type;
     }
 
     public enum Exception_Type {
